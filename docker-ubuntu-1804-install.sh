162dea3ed468126d469434a64485aa24814b485e
Nathan G <waveadeadchicken@yahoo.com>
2020-04-05 13:47:22 -0500

    basic Ubuntu 1804 docker install

---
+++ docker-ubuntu-1804-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt-get update
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
