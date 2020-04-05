c4ff68a836916b69be187baf3a621ee3463a8be7
Nathan G <waveadeadchicken@yahoo.com>
2020-04-05 16:58:50 -0500

    add the assume yes to automate installs

--- docker-ubuntu-1804-install.sh
+++ docker-ubuntu-1804-install.sh
@@ -2,14 +2,14 @@
 
 sudo apt-get update
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
+sudo apt-get update
 sudo apt-cache policy docker-ce
 sudo apt install docker-ce
 sudo systemctl enable docker
 sudo systemctl start docker
 
-sudo apt install python3-pip
+sudo apt-get -y install python3-pip
 sudo pip install docker-compose
